25809f90f2a1f3890db5382c63886cc02431c0aa
koylyakandriy <koylyakandriy@gmail.com>
2020-05-06 17:23:52 +0300

    add env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Backroads`,
@@ -29,7 +33,15 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-transition-link`
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENT_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENT_ACCESS_TOKEN,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import StyledHero from "../components/StyledHero"
+import Contact from "../components/Contact/Contact"
 
 export const data = graphql`
   {
@@ -21,6 +22,7 @@ const ContactPage = ({data}) => (
   <Layout>
     <SEO title="Contact" />
     <StyledHero img={data.contactBg.childImageSharp.fluid} />
+    <Contact />
   </Layout>
 )
 
